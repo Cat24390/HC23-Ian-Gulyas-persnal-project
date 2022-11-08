042b4adc67b8c9d1ab247598eacb5b10f832448a
Ian Gulyas <89816072+Cat24390@users.noreply.github.com>
2022-11-08 10:43:45 -0500

    Delete javaFXtest2 directory

--- javaFXtest2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.javafxtest2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.javafxtest2 to javafx.fxml;
-    exports com.example.javafxtest2;
-}
