b1451237ae690bd207e18bc7414c2c939f3ee28b
Ian Gulyas <IanGulyas@gmail.com>
2022-11-08 13:19:35 -0500

    work on java FX is now working within this file and added the options file

--- src/main/java/com/example/javafxtest2/HelloApplication.java
+++ src/main/java/com/example/javafxtest2/HelloApplication.java
@@ -19,23 +19,21 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) {
         TextField battleValue = new TextField("Enter Battle Value");
-        TextField resultfield = new TextField();
-        String fac[] ={"Draconis Combine","Federated Suns","Outworlds Alliance"};
-        String era[] ={"Third Sucession war","Clan Invasion","Illclan"};
-        ChoiceBox Factions = new ChoiceBox(FXCollections.observableArrayList(fac));
-        ChoiceBox Eras = new ChoiceBox(FXCollections.observableArrayList(era));
+        TextField result_field = new TextField();
+        ChoiceBox Factions = new ChoiceBox(FXCollections.observableArrayList(options.fac));
+        ChoiceBox Eras = new ChoiceBox(FXCollections.observableArrayList(options.era));
         Button exit = new Button("Exit");
         Button generate =new Button("Generate Force");
         generate.setOnAction(e -> {String val = battleValue.getText();
         String fact = String.valueOf(Factions.getValue());
         String age = String.valueOf(Eras.getValue());
-        resultfield.setText(fact + " " + val+ " "+ age);
+        result_field.setText(fact + " " + val+ " "+ age);
         });
         exit.setOnAction(e -> Platform.exit());
         VBox root = new VBox();
         root.setAlignment(Pos.CENTER);
         root.setSpacing(10);
-        root.getChildren().addAll(Factions,exit,generate,Eras,battleValue,resultfield);
+        root.getChildren().addAll(Factions,exit,generate,Eras,battleValue,result_field);
         Scene scene = new Scene(root, 500, 700);
         stage.setTitle("Hello!");
         stage.setScene(scene);
---
+++ src/main/java/com/example/javafxtest2/options.java
@@ -0,0 +1,7 @@
+package com.example.javafxtest2;
+
+public class options {
+    static public String fac[] ={"Draconis Combine","Federated Suns","","Outworlds Alliance"};
+    static public String era[] ={"Third Sucession war","Clan Invasion","Illclan"};
+
+}
