d82fec1dda890fd9a3bbc5eb65a33c9b3b2860a9
Ian Gulyas <IanGulyas@gmail.com>
2022-12-01 13:51:50 -0500

    Added "Save" feature, only works localy RN but we will get there Added Save Button to Hello Application

--- src/main/java/com/example/javafxtest2/HelloApplication.java
+++ src/main/java/com/example/javafxtest2/HelloApplication.java
@@ -3,28 +3,27 @@ package com.example.javafxtest2;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 // This is where the UI is created and set up.
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) {
+        int width = options.width;
+        int height = options.height;
         TextField battleValue = new TextField("Enter Battle Value");
         TextField result_field = new TextField();
         ChoiceBox Factions = new ChoiceBox(FXCollections.observableArrayList(options.fac));
         ChoiceBox Eras = new ChoiceBox(FXCollections.observableArrayList(options.era));
         Button exit = new Button("Exit");
         Button generate =new Button("Generate Force");
+        Button SaveButton = new Button("Save");
         generate.setOnAction(e -> {String val = battleValue.getText();
         String fact = String.valueOf(Factions.getValue());
         String age = String.valueOf(Eras.getValue());
@@ -33,11 +32,20 @@ public class HelloApplication extends Application {
         result_field.setText(result);
         });
         exit.setOnAction(e -> Platform.exit());
+        SaveButton.setOnAction(e ->
+        {
+            String val = battleValue.getText();
+            String fact = String.valueOf(Factions.getValue());
+            String age = String.valueOf(Eras.getValue());
+            Randomizer randomizer = new Randomizer(fact,val,age);
+            String result = randomizer.getResult();
+            Save.SaveData(result);
+        });
         VBox root = new VBox();
         root.setAlignment(Pos.CENTER);
         root.setSpacing(10);
-        root.getChildren().addAll(Factions,exit,generate,Eras,battleValue,result_field);
-        Scene scene = new Scene(root, 500, 700);
+        root.getChildren().addAll(Factions,exit,generate,Eras,battleValue,result_field,SaveButton);
+        Scene scene = new Scene(root, width, height);
         stage.setTitle("Random Force Generator");
         stage.setScene(scene);
         stage.show();
---
+++ src/main/java/com/example/javafxtest2/Save.java
@@ -0,0 +1,17 @@
+package com.example.javafxtest2;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Save {
+
+    public static void SaveData(String data) {
+        try {
+            Files.write(Paths.get("/Users/iangulyas/Desktop/SoftwareDeveloupment/HC23-Ian-Gulyas-Senior-Project/src/main/java/Save.txt"), data.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
