fed86ae04e86881f180736d9011afa16a3534e51
Ian Gulyas <IanGulyas@gmail.com>
2022-12-09 10:38:50 -0500

    Moved The Save File and updated it location in the Save class

--- src/main/java/com/example/RandomForceGenerator/Save.java
+++ src/main/java/com/example/RandomForceGenerator/Save.java
@@ -8,7 +8,7 @@ public class Save {
 
     public static void SaveData(String data) {
         try {
-            Files.write(Paths.get("/Users/iangulyas/Desktop/SoftwareDeveloupment/HC23-Ian-Gulyas-Senior-Project/src/main/java/Save.txt"), data.getBytes());
+            Files.write(Paths.get("/Users/iangulyas/Desktop/SoftwareDeveloupment/HC23-Ian-Gulyas-Senior-Project/Save.txt"), data.getBytes());
         } catch (IOException e) {
             e.printStackTrace();
         }
