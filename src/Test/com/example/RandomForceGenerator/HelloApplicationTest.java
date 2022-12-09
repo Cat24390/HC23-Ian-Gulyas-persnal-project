ded4fb8f4030d35b2f086a4b7f25b5d3c328c963
Ian Gulyas <IanGulyas@gmail.com>
2022-12-09 00:13:13 -0500

    Fixed and Updated Testing File

--- src/Test/com/example/RandomForceGenerator/HelloApplicationTest.java
+++ src/Test/com/example/RandomForceGenerator/HelloApplicationTest.java
@@ -11,13 +11,12 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
-//import org.testfx.assertions.api.Assertions;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import static org.junit.jupiter.api.Assertions.*;
-//import static org.testfx.api.FxAssert.verifyThat;
-//import static org.testfx.util.NodeQueryUtils.hasText;
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.util.NodeQueryUtils.hasText;
 
 //Where the Automatic Testing is done
 class HelloApplicationTest {
@@ -51,48 +50,87 @@ class HelloApplicationTest {
 
     }
 
-    public class HelloApplication extends Application {
+    public class RandomForceGenerator extends Application {
+        Button ExitButton;
+        Button generateButton;
+        Button SaveButton;
+        ChoiceBox FactionsChoiceBox;
+        ChoiceBox ErasChoiceBox;
+        TextField battleValueField;
+        TextField result_field;
         @Override
         public void start(Stage stage) {
-            int width = options.width;
-            int height = options.height;
-            TextField battleValue = new TextField("Enter Battle Value");
-            TextField result_field = new TextField();
-            ChoiceBox Factions = new ChoiceBox(FXCollections.observableArrayList(options.fac));
-            ChoiceBox Eras = new ChoiceBox(FXCollections.observableArrayList(options.era));
-            Button exit = new Button("Exit");
-            Button generate =new Button("Generate Force");
-            Button SaveButton = new Button("Save");
-            generate.setOnAction(e -> {String val = battleValue.getText();
-                String fact = String.valueOf(Factions.getValue());
-                String age = String.valueOf(Eras.getValue());
+            VBox UIroot = createAndIntiateUI();
+            Scene Mainscene = new Scene(UIroot, options.width,options.height);
+            stage.setTitle("Random Force Generator");
+            stage.setScene(Mainscene);
+            stage.show();
+        }
+
+        private VBox createAndIntiateUI() {
+            VBox UIroot = new VBox();
+            UIroot.setAlignment(Pos.CENTER);
+            UIroot.setSpacing(options.spacing);
+            AddUIelements(UIroot);
+            return UIroot;
+        }
+
+        private void AddUIelements(VBox UIroot) {
+            createButtons();
+            createTextFields();
+            createChoiceBoxes();
+            UIroot.getChildren().addAll(FactionsChoiceBox,ErasChoiceBox,battleValueField,generateButton,result_field,SaveButton,ExitButton);
+
+        }
+
+        private void createChoiceBoxes() {
+            FactionsChoiceBox = new ChoiceBox(FXCollections.observableArrayList(options.fac));
+            ErasChoiceBox = new ChoiceBox(FXCollections.observableArrayList(options.era));
+        }
+
+
+        private void createTextFields() {
+            battleValueField = new TextField("Enter Battle Value");
+            result_field = new TextField();
+        }
+
+        private void createButtons() {
+            ExitButton = new Button("Exit");
+            generateButton =new Button("Generate Force");
+            SaveButton = new Button("Save");
+            createExitButton(ExitButton);
+            createGenerateButton(generateButton);
+            createSaveButton(SaveButton);
+        }
+
+        private void createGenerateButton(Button generateButton) {
+            generateButton.setOnAction(e -> {String val = battleValueField.getText();
+                String fact = String.valueOf(FactionsChoiceBox.getValue());
+                String age = String.valueOf(ErasChoiceBox.getValue());
                 Randomizer randomizer = new Randomizer(fact,val,age);
                 String result = randomizer.getResult();
                 result_field.setText(result);
             });
-            exit.setOnAction(e -> Platform.exit());
-            SaveButton.setOnAction(e ->
+        }
+
+        private void createExitButton(Button exitButton) { exitButton.setOnAction(e -> Platform.exit());}
+
+        private void createSaveButton(Button saveButton) {
+            saveButton.setOnAction(e ->
             {
-                String val = battleValue.getText();
-                String fact = String.valueOf(Factions.getValue());
-                String age = String.valueOf(Eras.getValue());
+                String val = battleValueField.getText();
+                String fact = String.valueOf(FactionsChoiceBox.getValue());
+                String age = String.valueOf(ErasChoiceBox.getValue());
                 Randomizer randomizer = new Randomizer(fact,val,age);
                 String result = randomizer.getResult();
                 Save.SaveData(result);
             });
-            VBox root = new VBox();
-            root.setAlignment(Pos.CENTER);
-            root.setSpacing(10);
-            root.getChildren().addAll(Factions,exit,generate,Eras,battleValue,result_field,SaveButton);
-            Scene scene = new Scene(root, width, height);
-            stage.setTitle("Random Force Generator");
-            stage.setScene(scene);
-            stage.show();
         }
 
-       /* public void main(String[] args) {
+        public void main(String[] args) {
             launch();
         }
+
         @Test
         public void allUIButtonsAreHere()
         {
@@ -102,7 +140,7 @@ class HelloApplicationTest {
 
         }
 
-        */
+
     }
 
 
