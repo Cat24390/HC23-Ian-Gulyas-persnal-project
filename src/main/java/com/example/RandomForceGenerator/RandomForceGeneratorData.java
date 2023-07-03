430fd482f08ea867ec9b9ded19f8a4916a808ce5
Ian Gulyas <IanGulyas@gmail.com>
2023-07-03 18:09:03 -0400

    created a list of mechs because I am being stupid

--- src/main/java/com/example/RandomForceGenerator/RandomForceGeneratorData.java
+++ src/main/java/com/example/RandomForceGenerator/RandomForceGeneratorData.java
@@ -8,7 +8,13 @@ public class RandomForceGeneratorData {
     static public int width = 500;
     static public int height = 700;
     static public int spacing = 10;
-    static public String factions[] ={"Inner Sphere","Clan"};
-    static public String era[] ={"Pre StarLeague","StarLeague","The Succession wars","Clan Invasion","Dark Age","Ilclan"};
-
+    static public String factions[] = {"Inner Sphere", "Clan"};
+    static public String era[] = {"Pre StarLeague", "StarLeague", "The Succession wars", "Clan Invasion", "Dark Age", "Ilclan"};
+    static Mech mech1 = new Mech(1, "StarLeague", "Inner Sphere");
+    static Mech mech2 = new Mech(1, "The Succession wars", "Inner Sphere");
+    static Mech mech3 = new Mech(1, "Clan Invasion", "Inner Sphere");
+    static Mech mech4 = new Mech(1, "Dark Age", "Inner Sphere");
+    static Mech mech5 = new Mech(1, "Clan Invasion", "Clan");
+    static Mech mech6 = new Mech(1, "Dark Age", "Clan");
+    static public Mech Testmech[]= {mech1,mech2,mech3,mech4,mech5,mech6,};
 }
